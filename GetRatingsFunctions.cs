7b19ba169991d28906c14ed4d592f57ccd3a83c1
Will Li <luyuanli2010@gmail.com>
2021-06-07 16:21:29 -0700

    Add project files.

---
+++ GetRatingsFunctions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace BFYOC_api
+{
+    public static class GetRatingsFunctions
+    {
+        [FunctionName(nameof(GetRatings))]
+        public static IActionResult GetRatings(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            [CosmosDB(databaseName: "BFYOC", collectionName: "ratings", ConnectionStringSetting = "CosmosDBConnection")] IEnumerable<JObject> allRatings)
+        {
+            string userId = null;
+
+            if (req.GetQueryParameterDictionary()?.TryGetValue(@"userId", out userId) == true
+                && !string.IsNullOrWhiteSpace(userId))
+            {
+                var userRatings = allRatings.Where(r => r.Value<string>(@"userId") == userId);
+
+                return !userRatings.Any() ? new NotFoundObjectResult($@"No ratings found for user '{userId}'") : (IActionResult)new OkObjectResult(userRatings);
+
+            }
+            else
+            {
+                return new BadRequestObjectResult(@"userId is required as a query parameter");
+            }
+        }
+    }
+}
+
---
+++ Models/RatingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BFYOC_api.Models
+{
+    public class RatingModel
+    {
+        public string id { get; set; }
+        public string userId { get; set; }
+        public string productId { get; set; }
+        public DateTime timeStamp { get; internal set; }
+        public string locationName { get; set; }
+        public int rating { get; set; }
+        public string userNotes { get; set; }
+    }
+}
